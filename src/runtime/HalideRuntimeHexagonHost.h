f2e6b67f5a2dfda7ab96fc8d8d7541e6574b1a22
Suyog Sarda <ssarda@codeaurora.org>
2018-03-06 18:53:12 +0530

    Support more power levels for HVX
    add more power options for HVX.

--- src/runtime/HalideRuntimeHexagonHost.h
+++ src/runtime/HalideRuntimeHexagonHost.h
@@ -59,10 +59,17 @@ extern void halide_hexagon_power_hvx_off_as_destructor(void *user_context, void 
 
 /** Power modes for Hexagon. */
 typedef enum halide_hexagon_power_mode_t {
-    halide_hexagon_power_low     = 0,
-    halide_hexagon_power_nominal = 1,
-    halide_hexagon_power_turbo   = 2,
-    halide_hexagon_power_default = 3, /// Resets power to its default state.
+    halide_hexagon_power_low          = 0,
+    halide_hexagon_power_nominal      = 1,
+    halide_hexagon_power_turbo        = 2,
+    halide_hexagon_power_default      = 3, /// Resets power to its default state.
+    halide_hexagon_power_low_plus     = 4,
+    halide_hexagon_power_low_2        = 5,
+    halide_hexagon_power_nominal_plus = 6,
+
+    halide_hexagon_power_svs      = halide_hexagon_power_low,
+    halide_hexagon_power_svs_plus = halide_hexagon_power_low_plus,
+    halide_hexagon_power_svs_2    = halide_hexagon_power_low_2,
 
     // These are deprecated.
     halide_hvx_power_low     = halide_hexagon_power_low,
